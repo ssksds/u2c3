d70e69aeae1eaf02a51fce617e1be618e3258dd0
ssksds@b <99704118+ssksds@users.noreply.github.com>
2022-06-03 07:31:15 +0530

    Update movies.js

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,119 +1,75 @@
 // Implement debouncing for network request
 // On clicking book now store the selected movie in localstorage as key "movie"
 // so that you can retrive it on checkout.html page
- let purse=JSON.parse(localStorage.getItem("amount"));
- document.getElementById("wallet").innerText=purse;
+let purse = JSON.parse(localStorage.getItem("amount"));
+document.getElementById("wallet").innerText = purse;
 
+let id;
+
+let movies_div = document.getElementById("movies");
 
 async function searchMovies() {
-    try {
-      let movie = document.getElementById("query").value;
-  
-      let res = await fetch(
-        `https://www.omdbapi.com/?apikey=8eb1c82c&s=${movie}`
-      );
-  
-      let data = await res.json();
-  
-      console.log(data.Search);
-  
-      return data.Search;
-    } catch (err) {
-      console.log(err);
-    }
+  try {
+    let search = document.getElementById("search").value;
+    let res = await fetch(
+      `https://www.omdbapi.com/?apikey=8eb1c82c&s=${search}`
+    );
+    let data = await res.json();
+    let movies = data.Search;
+    return movies;
+  } catch (err) {
+    console.log("error", err);
   }
-  
-  async function main() {
-    let data = await searchMovies();
-  
-    if (data == undefined) {
-      return false;
-    }
-  
-    appendData(data);
-    renderingDom(data);
-    console.log(data);
-    console.log(data);
-  }
-  
-  let movies_div = document.getElementById("movies");
-  
-  let container = document.getElementById("container");
-  
-  function appendData(movies) {
-    movies_div.innerHTML = null;
-  
-    movies.forEach(function (element) {
-      let p = document.createElement("p");
-  
-      p.textContent = element.Title;
-  
-      movies_div.append(p);
+}
+
+function appendMovies(data) {
+  movies_div.innerHTML = null;
+  data.forEach((element) => {
+    let box = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = element.Poster;
+
+    let title = document.createElement("p");
+    title.innerText = element.Title;
+
+    let btn = document.createElement("button");
+    btn.innerText = "Book Now";
+    btn.setAttribute("class", "book_now");
+    btn.addEventListener("click", function () {
+      book(element);
     });
+
+    box.append(img, title, btn);
+    movies_div.append(box);
+  });
+}
+
+async function main() {
+  let data = await searchMovies();
+  // console.log(data);
+  if (data === undefined) {
+    return false;
   }
-  
-  //------------------------------------
-  
-  function renderingDom(data) {
-    container.innerHTML = null;
-  
-    data.forEach(function (element) {
-      let div1 = document.createElement("div");
-      div1.setAttribute("id", "div1");
-  
-      let div2 = document.createElement("div");
-      div2.setAttribute("id", "div2");
-  
-      let Poster = document.createElement("img");
-      Poster.src = element.Poster;
-  
-      let Title = document.createElement("h2");
-      Title.textContent = element.Title;
-  
-     
-  
-      let Released = document.createElement("p");
-      Released.textContent = "Released :" + " " + element.Year;
-  
-      
-  
-      let Genre = document.createElement("p");
-      Genre.textContent = "Genre :" + " " + element.Type;
-  
-      let Type = document.createElement("p");
-      Type.textContent = "Type :" + " " + element.Type;
-  
-      let Year = document.createElement("p");
-      Year.textContent = "Year :" + " " + element.Year;
-  
-     let button = document.createElement("button");
-     button.innerText="booknow"
-    //  btn.addEventListener("click", function () {
-    //   bookNow(elem);
-    //  });
-  
-      // let Ratings = document.createElement("p");
-      // Ratings.textContent = "Ratings :" + " " + element.Ratings[0].Value;
-  
-      div1.append(Poster);
-      div2.append(Title, Year, Released, Type, Genre,button);
-      container.append(div1, div2);
-    });
+  appendMovies(data);
+}
+
+function debouncing(func, delay) {
+  if (id) {
+    clearInterval(id);
   }
-  
-  //-----------------------------------
-  
-  
-  let timerId;
-  
-  function debounce(func, delay) {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-  
-    timerId = setTimeout(function () {
-      func();
-    }, delay); 
-  }
+  id = setTimeout(() => {
+    func();
+  }, delay);
+}
+
+let checkout = JSON.parse(localStorage.getItem("movie")) || [];
+function book(element) {
+  let obj = element;
+  checkout.push(obj);
+
+  localStorage.setItem("movie", JSON.stringify(checkout));
+
+  window.location.href = "checkout.html";
+}
   
   
